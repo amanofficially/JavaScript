de0c00050c452dd8f410b78cd8f12e473f677195
Aman Patel <amanofficially02@gmail.com>
2026-02-13 15:58:10 +0530

    Objects

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
-function userInfo() {
-  console.log(
-    `Username name is ${this.firstName} and he/she is ${this.age} year's old and he/she is from ${this.city}`,
-  );
+function createUser(firstName, lastName, age, city) {
+  const user = {};
+  user.firstName = firstName;
+  user.lastName = lastName;
+  user.age = age;
+  user.city = city;
+  user.about = function () {
+    return `Username is ${this.firstName} ${this.lastName} and he is ${this.age} year's old.`;
+  };
+  user.is18 = function () {
+    return this.age >= 18;
+  };
+
+  return user;
 }
-
-const user1 = {
-  firstName: "Aman",
-  age: 22,
-  city: "Indore",
-  about: userInfo,
-};
-const user2 = {
-  firstName: "Anand",
-  age: 21,
-  city: "Mumbai",
-  about: userInfo,
-};
-const user3 = {
-  firstName: "Aashu",
-  age: 21,
-  city: "Pune",
-  about: userInfo,
-};
-
-user1.about();
+const user2 = createUser("Anand", "Parmar", 22, "Indore");
+// const user1 = createUser("Aman", "Patel", 22, "Indore");
+console.log(user2);
+const user2Detail = user2.about();
+console.log(user2Detail);
+const is18 = user2.is18();
+console.log(is18);
