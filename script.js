6f07b66a34e2283c8133d28be441cdd5e52e74de
Aman Patel <amanofficially02@gmail.com>
2026-02-20 13:22:06 +0530

    Call,Apply,Bind function methods

--- script.js
+++ script.js
@@ -14,8 +14,27 @@
 // console.log(knowOddEven(2));
 
 //Arrow funtion
-let sayHello = () => {
-  return "Hello";
+// let sayHello = () => {
+//   return "Hello";
+// };
+
+// console.log(sayHello());
+
+// Call,Apply,Bind
+const user1 = {
+  fname: "Aman",
+  city: "Indore",
+  about: function (state, age) {
+    return `Hello my name is ${this.fname}, i'm ${age} years old. I'm from ${this.city}, ${state}.`;
+  },
 };
 
-console.log(sayHello());
+const user2 = {
+  fname: "Anand",
+  city: "Pune",
+};
+
+console.log(user1.about.call(user2, "Maharashtra"));
+console.log(user1.about.apply(user1, ["Madhya Pradesh", 22]));
+let result = user1.about.bind(user1, "Madhya Pradesh");
+result();
