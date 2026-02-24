a30fe7fc9a60003f1f0df5f48c980e141c421515
Aman Patel <amanofficially02@gmail.com>
2026-02-24 13:49:06 +0530

    Practise

--- practise.js
+++ practise.js
@@ -1,45 +1,70 @@
-// const user1 = {
-//   name: "Aman",
-//   age: 22,
-//   id: 401,
-// };
-// const user2 = {
-//   name: "Anand",
-//   age: 21,
-//   id: 406,
-// };
-// const userMethods = {
-//   about: function () {
-//     console.log(`Hello i'm ${this.name} and i'm ${this.age} years old.`);
-//   },
-//   is18: function () {
+// // const user1 = {
+// //   name: "Aman",
+// //   age: 22,
+// //   id: 401,
+// // };
+// // const user2 = {
+// //   name: "Anand",
+// //   age: 21,
+// //   id: 406,
+// // };
+// // const userMethods = {
+// //   about: function () {
+// //     console.log(`Hello i'm ${this.name} and i'm ${this.age} years old.`);
+// //   },
+// //   is18: function () {
+// //     return this.age >= 18;
+// //   },
+// //   sing: function () {
+// //     console.log(`User is singning..`);
+// //   },
+// // };
+
+// class CreateUser {
+//   constructor(name, age, id) {
+//     this.name = name;
+//     this.age = age;
+//     this.id = id;
+//   }
+//   about() {
+//     console.log(`Hello i'm ${this.name}.`);
+//   }
+
+//   is18() {
 //     return this.age >= 18;
-//   },
-//   sing: function () {
+//   }
+//   sing() {
 //     console.log(`User is singning..`);
-//   },
-// };
+//   }
+// }
 
-function createUser(name, age, id) {
-  // const user = Object.create(createUser.prototype);
-  this.name = name;
-  this.age = age;
-  this.id = id;
-}
-
-createUser.prototype.about = function () {
-  console.log(`Hello i'm ${this.name}.`);
-};
-
-createUser.prototype.is18 = function () {
-  return this.age >= 18;
-};
-createUser.prototype.sing = function () {
-  console.log(`User is singning..`);
-};
-
-let user1 = new createUser("Aman", 22, 401);
+// let user1 = new CreateUser("Aman", 22, 401);
 // console.log(user1);
-user1.about();
-console.log(user1.is18());
-user1.sing();
+// user1.about();
+// // function CreateUser(name, age, id) {
+// //   // const user = Object.create(createUser.prototype);
+// //   this.name = name;
+// //   this.age = age;
+// //   this.id = id;
+// // }
+
+// // CreateUser.prototype.about = function () {
+// //   console.log(`Hello i'm ${this.name}.`);
+// // };
+
+// // CreateUser.prototype.is18 = function () {
+// //   return this.age >= 18;
+// // };
+// // CreateUser.prototype.sing = function () {
+// //   console.log(`User is singning..`);
+// // };
+
+// // let user1 = new CreateUser("Aman", 16, 401);
+// // // console.log(user1);
+// // console.log(CreateUser.prototype === user1.__proto__);
+// // for (let key in user1) {
+// //   console.log(key);
+// // }
+// // user1.about();
+// // console.log(user1.is18());
+// // user1.sing();
